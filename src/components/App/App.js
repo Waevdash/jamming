5479cbf6ab3771b61cf488ed5d0206c9c793fcc1
Ryan Wells <ryanwells21@yahoo.com>
2017-12-08 00:40:58 -0800

    first draft done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist'
 import SearchBar from '../SearchBar/SearchBar'
+import Spotify from '../../util/Spotify'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchResults: [],
+      searchResults: [{track}],
       playlistName: 'My Playlist',
       playlistTracks: []
     };
@@ -20,7 +20,7 @@ class App extends Component {
     this.search = this.search.bind(this)
   }
   addTrack(track) {
-    //his will work through the array, then return `true` if it finds at least one case where `t.id === track.id`
+    //This will work through the array, then return `true` if it finds at least one case where `t.id === track.id`
     if (!this.state.playlistTracks.id.some(t => t.id === track.id)) {
         let pushTrack = this.state.playlistTrack;
         pushTrack.push(track);
@@ -43,10 +43,15 @@ class App extends Component {
     })
   }
   savePlaylist(){
-    let trackURIs = []
+    let trackURIs = [];
+    Spotify.savePlaylist();
+    this.setState({
+      searchResults: []
+      playlistName: 'My Playlist';
+    })
   }
   search(searchTerm){
-    console.log(searchTerm)
+    Spotify.search(searchTerm)
   }
   render() {
     return (
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 let clientId = '3752d707ec8d4097a81ec5f728e8d036'
 let redirect_uri = 'http://localhost:3000/'
 let accessToken = undefined
@@ -8,7 +9,7 @@ const Spotify = {
     if (accessToken){
       return new Promise(resolve => resolve(accessToken));
     } else {
-      const spotifyURL = `https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirect_uri}&scope=playlist-modify-public&response_type=token$state=123`
+      const spotifyAuthorizationURL = `https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirect_uri}&scope=playlist-modify-public&response_type=token$state=123`
       const accessTokenExist = window.location.href.match('/access_token=([^&]*)/');
       const expiresInExist = window.location.href.match('/expires_in=([^&]*)/');
       if (accessTokenExist && expiresInExist) {
@@ -17,8 +18,52 @@ const Spotify = {
         window.setTimeout(() => accessToken = '', expiresIn * 1000);
         window.history.pushState('Access Token', null, '/');
       }
-      window.location = spotifyURL
+      window.location = spotifyAuthorizationURL
     }
+  },
+  search(searchTerm){
+    return new Promise(fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).then(response => {return response.json}).then(jsonResponse => {
+      if (jsonResponse.tracks) {
+        return jsonResponse.tracks.map(track => {
+          id: track.id;
+          name: track.name;
+          artist: track.artists[0].name;
+          album: track.album.name;
+          URI: track.uri;
+        })
+      } else {
+        return jsonResponse.tracks.map(track => {[]})
+        }
+    }))
+  },
+  savePlaylist(playlistName,trackURIs){
+    if (playlistName && trackURIs) {
+      const accessTokenSave = window.location.href.match('/access_token=([^&]*)/');
+      const spotifyAuthorizationURL = `https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirect_uri}&scope=playlist-modify-public&response_type=token$state=123`;
+      let userID = undefined;
+      let playlistID = undefined;
+      return new Promise(fetch(`https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/me`, {
+        headers: {Authorization: `Bearer ${accessToken}`}
+      }).then(response => {return response.json}).then(jsonResponse => {
+        return (userID = jsonResponse.id)
+      });
+      new Promise(fetch(`https://cors-anywhere.herokuapp.com//v1/users/${userID}/playlists`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+          Content-type: "application/json"
+        },
+        body: {
+          name: {playlistName}
+        },
+      })).then(response => {return repsonse.json}).then(jsonResponse => {
+        return (playlistID = jsonResponse.id)
+      })
+    } else {return}
+    window.location = spotifyAuthorizationURL
+
   }
 }
 
